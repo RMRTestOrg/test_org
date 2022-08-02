9d33ee821dc653e0adfc9251351d52cc82f68ea0
Ryan Richard <ryanmrichard1@gmail.com>
2022-08-02 14:24:08 -0500

    make it look like CMinx

---
+++ install/lib/cminx/cmake/cminx-config.cmake
@@ -0,0 +1,56 @@
+# Copyright 2022 CMakePP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was cminx-config.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+# Find the CMinx binary
+set_and_check(CMINX_BIN_DIR "${PACKAGE_PREFIX_DIR}/bin")
+find_program(CMINX_EXECUTABLE_FOUND cminx "${CMINX_BIN_DIR}" REQUIRED)
+set(CMINX_EXECUTABLE ${CMINX_BIN_DIR}/cminx)
+
+# Find the CMinx CMake module
+set_and_check(CMINX_CMAKE_DIR "${PACKAGE_PREFIX_DIR}/lib/cminx/cmake")
+find_file(
+    CMINX_CMAKE_MODULE_FOUND cminx.cmake 
+    HINTS "${CMINX_CMAKE_DIR}" 
+    REQUIRED
+)
+
+check_required_components(cminx)
+
+include("${CMINX_CMAKE_DIR}/cminx.cmake")
